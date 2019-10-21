61cefecff078cdb12e1ab072af23d934f72381db
Aquib Vadsaria <vadsaria.ak@gmail.com>
2019-10-21 18:11:41 +0530

    Updates

---
+++ component---src-pages-404-js-b277a5a038d3c4a5cf25.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{216:function(t,e,n){"use strict";n.r(e);var u=n(1),o=(n(0),n(215)),s=n(214);e.default=function(){return Object(u.b)(o.a,null,Object(u.b)(s.a,{title:"404: Not found"}),Object(u.b)("h1",null,"NOT FOUND"),Object(u.b)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b277a5a038d3c4a5cf25.js.map
