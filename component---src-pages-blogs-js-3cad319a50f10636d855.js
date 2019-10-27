9abf9bb8f33fc1554b8f388f6aeb725ae5a10c38
Aquib Vadsaria <vadsaria.ak@gmail.com>
2019-10-28 00:16:25 +0530

    Updates

---
+++ component---src-pages-blogs-js-3cad319a50f10636d855.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{223:function(t,a,b){"use strict";b.r(a);var n=b(1),e=(b(0),b(53)),o=b(218),c=b(217);a.default=function(){return Object(n.b)(o.a,{page:"Blogs"},Object(n.b)(c.a,{title:"Aquib Vadsaria blogs"}),Object(n.b)("h1",null,"Blogs"),Object(n.b)(e.Link,{to:"/"},"Back to Main"))}}}]);
+//# sourceMappingURL=component---src-pages-blogs-js-3cad319a50f10636d855.js.map
