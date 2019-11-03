0a54ede31b7c9e61da2576f345b70628c1d13f17
Aquib Vadsaria <vadsaria.ak@gmail.com>
2019-11-04 01:40:46 +0530

    fix: experience timeline background and include company name as main Title

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -26,6 +26,14 @@ const ExperienceContainer = styled.div`
       color: white;
     }
   }
+
+  .vertical-timeline-element-content,
+  .vertical-timeline::before {
+    background: #cfd8dc;
+  }
+  .vertical-timeline-element-content-arrow {
+    border-right-color: #cfd8dc;
+  }
   .vertical-timeline-element-title {
     padding-bottom: 5px;
     @media (max-width: 768px) {
@@ -57,10 +65,10 @@ const Experience = () => (
           iconStyle={{ background: "black", color: "#fff" }}
           icon={<BrandIcon src={jpmorgan} alt={"JP Morgan icon"} />}
         >
-          <h3 className="vertical-timeline-element-title">
+          <h3 className="vertical-timeline-element-title">J.P Morgan Chase</h3>
+          <h5 className="vertical-timeline-element-subtitle">
             Application Developer
-          </h3>
-          <h5 className="vertical-timeline-element-subtitle">Mumbai, India</h5>
+          </h5>
           <p className="vertical-timeline-element-description">
             Working on a trading platform, rebuilding current Flex application
             to HTML/JS based application
@@ -72,8 +80,8 @@ const Experience = () => (
           iconStyle={{ background: "#fff", color: "#fff" }}
           icon={<BrandIcon src={builtIo} alt={"Built.io icon"} />}
         >
-          <h3 className="vertical-timeline-element-title">Web Engineer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Mumbai, India</h4>
+          <h3 className="vertical-timeline-element-title">Built.io</h3>
+          <h4 className="vertical-timeline-element-subtitle">Web Engineer</h4>
           <p className="vertical-timeline-element-description">
             Worked as a full stack developer for{" "}
             <a href="https://www.built.io/products/flow/features">
@@ -88,8 +96,10 @@ const Experience = () => (
           iconStyle={{ background: "#fff", color: "#fff" }}
           icon={<BrandIcon src={ibm} alt={"IBM icon"} />}
         >
-          <h3 className="vertical-timeline-element-title">System Engineer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Mumbai, India</h4>
+          <h3 className="vertical-timeline-element-title">IBM</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            System Engineer
+          </h4>
           <p className="vertical-timeline-element-description">
             Worked as System engineer where I used to setup, configure mainframe
             servers, swicthes and storages mainly used for high computing nodes
